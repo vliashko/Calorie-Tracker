3ce47a6a3c1bf41f55110929473497b5af09d2d6
vlyashko02 <vladimir.lyashko02@gmail.com>
2021-04-12 10:37:12 +0300

    Добавлено логирование (NLog)

---
+++ WebApiCT/Contracts/ILoggerManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts
+{
+    public interface ILoggerManager
+    {
+        void LogError(string message);
+        void LogDebug(string message);
+        void LogInfo(string message);
+        void LogWarn(string message);
+    }
+}
